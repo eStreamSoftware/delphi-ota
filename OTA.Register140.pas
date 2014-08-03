62ea12954ce0b0690379f110e94c03b0807d6173
Chau Chee Yang <ccy@sql.com.my>
2014-08-03 15:42:34 +0800

    Remove OTA context menu of ProjectGroup and Project build configuration

--- OTA.Register140.pas
+++ OTA.Register140.pas
@@ -4,14 +4,12 @@ interface
 
 implementation
 
-uses OTA.IDE, OTA.IDE140, OTA.QC92507, OTA.DataSnap.ProxyClient, OTA.ConfigurationBuild;
+uses OTA.IDE, OTA.IDE140, OTA.QC92507, OTA.DataSnap.ProxyClient;
 
 initialization
   TOTAFactory
     .Register(TNotifierOTA_ProjectManager_140.Create(T_QC92507_BaseConfigurationContainer))
     .Register(TNotifierOTA_ProjectManager_140.Create(T_QC92507_BuildConfigContainer))
     .Register(TNotifierOTA_ProjectManager_140.Create(TAddInterface_DataSnap_ProxyClient))
-    .Register(TNotifierOTA_ProjectManager_140.Create(T_ProjectGroup_ConfigurationBuild))
-    .Register(TNotifierOTA_ProjectManager_140.Create(T_Project_ConfigurationBuild))
     ;
 end.
