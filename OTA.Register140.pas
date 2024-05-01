73bf3b1dbd946717f98249c7523fd4b0b7f8e472
Chau Chee Yang <ccy@sql.com.my>
2024-05-01 14:14:04 +0800

    Remove TAddInterface_DataSnap_ProxyClient support

--- OTA.Register140.pas
+++ OTA.Register140.pas
@@ -5,11 +5,10 @@ interface
 implementation
 
 uses
-  OTA.DataSnap.ProxyClient, OTA.FormatUses, OTA.IDE, OTA.IDE140;
+  OTA.FormatUses, OTA.IDE, OTA.IDE140;
 
 initialization
   TOTAFactory
-    .Register(TNotifierOTA_ProjectManager_140.Create(TAddInterface_DataSnap_ProxyClient))
     .Register(TNotifierOTA_ProjectManager_140.Create(TProject_FormatUses))
     ;
 end.
