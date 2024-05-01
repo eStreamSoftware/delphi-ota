85fb21c07ab8f4d13be31193c5bd03720689647d
Chau Chee Yang <ccy@sql.com.my>
2024-05-01 14:22:17 +0800

    Remove TSetOEMDir support

--- OTA.Register110.pas
+++ OTA.Register110.pas
@@ -5,17 +5,12 @@ interface
 implementation
 
 uses OTA.IDE, OTA.IDE110,
-     OTA.BuildAllFromHere, OTA.SearchMissingFile, OTA.SearchProject
-     {$if CompilerVersion = 18.5}, OTA.SetOEMDir {$ifend}
-     ;
+     OTA.BuildAllFromHere, OTA.SearchMissingFile, OTA.SearchProject;
 
 initialization
   TOTAFactory
     .Register(TNotifierOTA_ProjectManager_110.Create(TBuildAllFromHere))
     .Register(TNotifierOTA_ProjectManager_110.Create(TSearchMissingFile))
     .Register(TNotifierOTA_ProjectManager_110.Create(TSearchProject))
-    {$if CompilerVersion = 18.5}
-      .Register(TNotifierOTA_Services.Create(TSetOEMDir))
-    {$ifend}
-    ;
+  ;
 end.
